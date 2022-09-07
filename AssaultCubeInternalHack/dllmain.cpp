b1a3501363365da71e8118dab55efef55c22dc11
Daniel Pyon <pyondohyun@gmail.com>
2022-09-07 11:33:02 -0700

    dll injection works

--- AssaultCubeInternalHack/dllmain.cpp
+++ AssaultCubeInternalHack/dllmain.cpp
@@ -10,6 +10,7 @@ DWORD WINAPI HackThread(HMODULE hModule) {
 	
 	FILE* f;
 	freopen_s(&f, "CONOUT%", "w", stdout);
+	std::cout << "started" << std::endl;
 
 	uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
 
@@ -65,8 +66,7 @@ DWORD WINAPI HackThread(HMODULE hModule) {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH: {
-		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr);
-
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
 	}
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
